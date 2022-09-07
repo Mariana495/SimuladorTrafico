3635e79ebbdf55a5b7b6b991352c65de4268213b
Mrjojosa <88664775+Mrjojosa@users.noreply.github.com>
2022-09-07 12:09:53 -0500

    Add files via upload

--- Assets/Scripts/AttrDZ.cs
+++ Assets/Scripts/AttrDZ.cs
@@ -14,31 +14,6 @@ public class AttrDZ : MonoBehaviour
 
     private void Update()
     {
-        if( nBoxes < 5)
-        {
-            if(Physics.Raycast(new Vector3(transform.position.x, 1.8f, transform.position.z), new Vector3(0, 1, 2), out hit, 6f) && hit.transform.tag == "Rob")
-            {
-                Debug.Log(hit.transform.position);
-                possibleBox = hit.transform.gameObject.GetComponent<Move>().numBox;
-                if(possibleBox > 0)
-                {
-                    if((nBoxes + possibleBox) > 5)
-                    {
-                        int tempRest = 5 - nBoxes;
-                        possibleBox = possibleBox - tempRest;
-                        nBoxes = 5;
-                        Debug.Log(nBoxes);
-                    }
-                    else
-                    {
-                        nBoxes = nBoxes + possibleBox;
-                        possibleBox = 0;
-                        Debug.Log(nBoxes);
-                    }
-                    hit.transform.gameObject.GetComponent<Move>().numBox = possibleBox;
-                }
-            }
-        }
         if (nBoxes > 0)
         {
             Instantiate(decor, new Vector3(transform.position.x, 1.90f, transform.position.z), Quaternion.identity);
@@ -49,5 +24,27 @@ public class AttrDZ : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, new Vector3(0,1,2) * 6f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        possibleBox = other.GetComponent<Move>().numBox;
+        if(possibleBox > 0)
+        {
+            if( nBoxes < 5)
+            {
+                if((nBoxes + possibleBox) > 5)
+                {
+                    int tempRest = 5 - nBoxes;
+                    possibleBox = possibleBox - tempRest;
+                    nBoxes = 5;
+                }
+                else
+                {
+                    nBoxes = nBoxes + possibleBox;
+                    possibleBox = 0;
+                }
+            }other.GetComponent<Move>().numBox = possibleBox;
+        }
     }
 }
